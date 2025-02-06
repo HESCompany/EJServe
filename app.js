1ff78c8097d3aee160aa1a0c8fc0db60cb67457e
HESCO <haryawan147@gmail.com>
2025-02-06 10:31:32 +0700

    MUPD0FIX2

--- app.js
+++ app.js
@@ -9,7 +9,8 @@ const db = mysql.createConnection({
   host: process.env.MYSQLHOST,
   user: process.env.MYSQLUSER,
   password: process.env.MYSQLPASSWORD,
-  database: process.env.MYSQLDATABASE
+  database: process.env.MYSQLDATABASE,
+  port: process.env.MYSQLPORT
 });
 
 db.connect(err => {
